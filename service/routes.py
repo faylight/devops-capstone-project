d851eb20ae6dd4128db846b8ca7b8a0d34714b92
faylight <acc.tiffany95@gmail.com>
2024-12-13 13:43:15 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -56,6 +56,7 @@ def create_accounts():
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
+    
 
 ######################################################################
 # LIST ALL ACCOUNTS
@@ -73,6 +74,7 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -88,6 +90,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+    
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -107,6 +110,7 @@ def update_accounts(account_id):
     account.update()
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -122,6 +126,7 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
